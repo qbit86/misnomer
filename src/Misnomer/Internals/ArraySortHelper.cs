ad380ce425f9b133bf4c5c05ba86b0fbff80b8b3
Viktor T <qbit86@gmail.com>
2018-04-19 01:30:06 +0300

    Moved `ArraySortHelper` to Internals from Fakes.

--- src/Misnomer/Internals/ArraySortHelper.cs
+++ src/Misnomer/Internals/ArraySortHelper.cs
@@ -2,11 +2,9 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-using System.Collections.Generic;
 using System.Diagnostics;
 
-namespace Misnomer
+namespace System.Collections.Generic
 {
     #region ArraySortHelper for single arrays
 
