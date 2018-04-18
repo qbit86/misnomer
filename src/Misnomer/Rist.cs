c06c8b40741d99a13e001613c6aeec9820172943
Viktor T <qbit86@gmail.com>
2018-04-19 01:36:17 +0300

    Updated Rist from original source.

--- src/Misnomer/Rist.cs
+++ src/Misnomer/Rist.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
@@ -17,7 +17,7 @@ namespace Misnomer
     // of the internal array. As elements are added to a List, the capacity
     // of the List is automatically increased as required by reallocating the
     // internal array.
-    //
+    // 
     [DebuggerTypeProxy(typeof(ICollectionDebugView<>))]
     [DebuggerDisplay("Count = {Count}")]
     [Serializable]
@@ -46,7 +46,7 @@ namespace Misnomer
         // Constructs a List with a given initial capacity. The list is
         // initially empty, but will have room for the given number of elements
         // before any reallocations are required.
-        //
+        // 
         public List(int capacity)
         {
             if (capacity < 0)
@@ -61,7 +61,7 @@ namespace Misnomer
         // Constructs a List, copying the contents of the given collection. The
         // size and capacity of the new list will both be equal to the size of the
         // given collection.
-        //
+        // 
         public List(IEnumerable<T> collection)
         {
             if (collection == null)
@@ -90,9 +90,9 @@ namespace Misnomer
         }
 
         // Gets and sets the capacity of this list.  The capacity is the size of
-        // the internal array used to hold items.  When set, the internal
+        // the internal array used to hold items.  When set, the internal 
         // array of the list is reallocated to the given capacity.
-        //
+        // 
         public int Capacity
         {
             get
@@ -178,7 +178,7 @@ namespace Misnomer
         private static bool IsCompatibleObject(object value)
         {
             // Non-null values are fine.  Only accept nulls if T is a class or Nullable<U>.
-            // Note that default(T) is not equal to null for value types except when T is Nullable<U>.
+            // Note that default(T) is not equal to null for value types except when T is Nullable<U>. 
             return ((value is T) || (value == null && default(T) == null));
         }
 
@@ -276,10 +276,10 @@ namespace Misnomer
         // is larger than the given search value. This is also the index at which
         // the search value should be inserted into the list in order for the list
         // to remain sorted.
-        //
+        // 
         // The method uses the Array.BinarySearch method to perform the
         // search.
-        //
+        // 
         public int BinarySearch(int index, int count, T item, IComparer<T> comparer)
         {
             if (index < 0)
@@ -360,13 +360,13 @@ namespace Misnomer
             return list;
         }
 
-        // Copies this List into array, which must be of a
-        // compatible array type.
+        // Copies this List into array, which must be of a 
+        // compatible array type.  
         public void CopyTo(T[] array)
             => CopyTo(array, 0);
 
-        // Copies this List into array, which must be of a
-        // compatible array type.
+        // Copies this List into array, which must be of a 
+        // compatible array type.  
         void ICollection.CopyTo(Array array, int arrayIndex)
         {
             if ((array != null) && (array.Rank != 1))
@@ -386,9 +386,9 @@ namespace Misnomer
         }
 
         // Copies a section of this list to the given array at the given index.
-        //
+        // 
         // The method uses the Array.Copy method to copy the elements.
-        //
+        // 
         public void CopyTo(int index, T[] array, int arrayIndex, int count)
         {
             if (_size - index < count)
@@ -580,8 +580,8 @@ namespace Misnomer
         }
 
         // Returns an enumerator for this list with the given
-        // permission for removal of elements. If modifications made to the list
-        // while an enumeration is in progress, the MoveNext and
+        // permission for removal of elements. If modifications made to the list 
+        // while an enumeration is in progress, the MoveNext and 
         // GetObject methods of the enumerator will throw an exception.
         //
         public Enumerator GetEnumerator()
@@ -621,10 +621,10 @@ namespace Misnomer
         // this list. The list is searched forwards from beginning to end.
         // The elements of the list are compared to the given value using the
         // Object.Equals method.
-        //
+        // 
         // This method uses the Array.IndexOf method to perform the
         // search.
-        //
+        // 
         public int IndexOf(T item)
             => Array.IndexOf(_items, item, 0, _size);
 
@@ -642,10 +642,10 @@ namespace Misnomer
         // index and ending at count number of elements. The
         // elements of the list are compared to the given value using the
         // Object.Equals method.
-        //
+        // 
         // This method uses the Array.IndexOf method to perform the
         // search.
-        //
+        // 
         public int IndexOf(T item, int index)
         {
             if (index > _size)
@@ -658,10 +658,10 @@ namespace Misnomer
         // index and upto count number of elements. The
         // elements of the list are compared to the given value using the
         // Object.Equals method.
-        //
+        // 
         // This method uses the Array.IndexOf method to perform the
         // search.
-        //
+        // 
         public int IndexOf(T item, int index, int count)
         {
             if (index > _size)
@@ -676,7 +676,7 @@ namespace Misnomer
         // Inserts an element into this list at a given index. The size of the list
         // is increased by one. If required, the capacity of the list is doubled
         // before inserting the new element.
-        //
+        // 
         public void Insert(int index, T item)
         {
             // Note that insertions at the end are legal.
@@ -771,13 +771,13 @@ namespace Misnomer
         }
 
         // Returns the index of the last occurrence of a given value in a range of
-        // this list. The list is searched backwards, starting at the end
-        // and ending at the first element in the list. The elements of the list
+        // this list. The list is searched backwards, starting at the end 
+        // and ending at the first element in the list. The elements of the list 
         // are compared to the given value using the Object.Equals method.
-        //
+        // 
         // This method uses the Array.LastIndexOf method to perform the
         // search.
-        //
+        // 
         public int LastIndexOf(T item)
         {
             if (_size == 0)
@@ -792,13 +792,13 @@ namespace Misnomer
 
         // Returns the index of the last occurrence of a given value in a range of
         // this list. The list is searched backwards, starting at index
-        // index and ending at the first element in the list. The
-        // elements of the list are compared to the given value using the
+        // index and ending at the first element in the list. The 
+        // elements of the list are compared to the given value using the 
         // Object.Equals method.
-        //
+        // 
         // This method uses the Array.LastIndexOf method to perform the
         // search.
-        //
+        // 
         public int LastIndexOf(T item, int index)
         {
             if (index >= _size)
@@ -811,10 +811,10 @@ namespace Misnomer
         // index and upto count elements. The elements of
         // the list are compared to the given value using the Object.Equals
         // method.
-        //
+        // 
         // This method uses the Array.LastIndexOf method to perform the
         // search.
-        //
+        // 
         public int LastIndexOf(T item, int index, int count)
         {
             if ((Count != 0) && (index < 0))
@@ -990,7 +990,7 @@ namespace Misnomer
             _version++;
         }
 
-        // Sorts the elements in this list.  Uses the default comparer and
+        // Sorts the elements in this list.  Uses the default comparer and 
         // Array.Sort.
         public void Sort()
             => Sort(0, Count, null);
@@ -1005,9 +1005,9 @@ namespace Misnomer
         // comparer is null, the elements are compared to each other using
         // the IComparable interface, which in that case must be implemented by all
         // elements of the list.
-        //
+        // 
         // This method uses the Array.Sort method to sort the elements.
-        //
+        // 
         public void Sort(int index, int count, IComparer<T> comparer)
         {
             if (index < 0)
@@ -1063,10 +1063,10 @@ namespace Misnomer
         // new elements will be added to the list. To completely clear a list and
         // release all memory referenced by the list, execute the following
         // statements:
-        //
+        // 
         // list.Clear();
         // list.TrimExcess();
-        //
+        // 
         public void TrimExcess()
         {
             int threshold = (int)(((double)_items.Length) * 0.9);
