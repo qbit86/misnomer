da8d4c32e13c0ad885cddcfc2e30ec6bcdf7df04
Viktor T <qbit86@gmail.com>
2018-04-19 01:27:06 +0300

    Moved `StackTraceHiddenAttribute` to Internals from Fakes.

--- src/Misnomer/Internals/StackTraceHiddenAttribute.cs
+++ src/Misnomer/Internals/StackTraceHiddenAttribute.cs
@@ -2,9 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 // See the LICENSE file in the project root for more information.
 
-using System;
-
-namespace Misnomer
+namespace System.Diagnostics
 {
     [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method | AttributeTargets.Constructor | AttributeTargets.Struct, Inherited = false)]
     internal sealed class StackTraceHiddenAttribute : Attribute
