fe3cf5ef7f264f5b2bbb33c57a7e1ec9546177c6
Viktor T <qbit86@gmail.com>
2018-04-19 02:29:32 +0300

    Added couple of constants.

--- src/Misnomer/Fakes/SR.Partial.cs
+++ src/Misnomer/Fakes/SR.Partial.cs
@@ -1,9 +1,11 @@
 ﻿// ReSharper disable once CheckNamespace
 namespace System
 {
+    // https://github.com/dotnet/corert/blob/master/src/System.Private.CoreLib/src/Resources/Strings.resx
     // ReSharper disable once InconsistentNaming
     internal static partial class SR
     {
-
+        internal const string Arg_BogusIComparer = "Unable to sort because the IComparer.Compare() method returns inconsistent results. Either a value does not compare equal to itself, or one value repeatedly compared to another value yields different results. IComparer: '{0}'.";
+        internal const string InvalidOperation_IComparerFailed = "Failed to compare two elements in the array.";
     }
 }
