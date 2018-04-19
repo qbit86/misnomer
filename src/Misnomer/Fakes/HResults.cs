58fb32736b709c6514f0a8fcc9f24a7e65eec2c8
Viktor T <qbit86@gmail.com>
2018-04-19 14:25:25 +0300

    Added `HResults`.

---
+++ src/Misnomer/Fakes/HResults.cs
@@ -0,0 +1,11 @@
+﻿// ReSharper disable InconsistentNaming
+// ReSharper disable once CheckNamespace
+namespace System
+{
+    // https://github.com/dotnet/corert/blob/master/src/System.Private.CoreLib/shared/System/HResults.cs
+    internal static class HResults
+    {
+        internal const int COR_E_SERIALIZATION = unchecked((int)0x8013150C);
+        internal const int COR_E_SYSTEM = unchecked((int)0x80131501);
+    }
+}
