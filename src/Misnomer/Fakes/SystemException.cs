4e1406e2a1f7efc4361bd8d5ac96956eacc473af
Viktor T <qbit86@gmail.com>
2018-04-19 09:46:59 +0300

    Added exceptions (not compiling yet).

---
+++ src/Misnomer/Fakes/SystemException.cs
@@ -0,0 +1,34 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+
+namespace Misnomer
+{
+    [Serializable]
+    public class SystemException : Exception
+    {
+        public SystemException()
+            : base(SR.Arg_SystemException)
+        {
+            HResult = HResults.COR_E_SYSTEM;
+        }
+
+        public SystemException(String message)
+            : base(message)
+        {
+            HResult = HResults.COR_E_SYSTEM;
+        }
+
+        public SystemException(String message, Exception innerException)
+            : base(message, innerException)
+        {
+            HResult = HResults.COR_E_SYSTEM;
+        }
+
+        protected SystemException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
