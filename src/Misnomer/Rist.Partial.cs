aa84fc8064281dbca8e9b0818a87a93659e199bf
Viktor T <qbit86@gmail.com>
2018-04-20 02:19:25 +0300

    Implemented IDisposable.

--- src/Misnomer/Rist.Partial.cs
+++ src/Misnomer/Rist.Partial.cs
@@ -1,8 +1,19 @@
-﻿namespace Misnomer
+﻿using System;
+
+namespace Misnomer
 {
     // https://github.com/dotnet/corert/blob/master/src/System.Private.CoreLib/shared/System/Collections/Generic/List.cs
-    public partial class Rist<T>
+    public partial class Rist<T> : IDisposable
     {
         private const int MaxArrayLength = 0X7FEFFFFF;
+
+        private System.Buffers.ArrayPool<T> Pool { get; } = System.Buffers.ArrayPool<T>.Shared;
+
+        public void Dispose()
+        {
+            T[] oldItems = _items;
+            _items = s_emptyArray;
+            Pool.Return(oldItems);
+        }
     }
 }
