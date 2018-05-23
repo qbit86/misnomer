899f920d18289bd6bf2db6f2fceb31d22004d68b
Viktor T <qbit86@gmail.com>
2018-05-23 23:28:38 +0300

    Added Capacity test.

--- test/Misnomer.Test/RistTest.cs
+++ test/Misnomer.Test/RistTest.cs
@@ -9,10 +9,24 @@ namespace Misnomer
     public sealed class RistTest
     {
         [Fact]
+        public void Capacity_ShouldBeEnough()
+        {
+            // Arrange
+            var rist = new Rist<int> {21, 2, 8, 5, 3, 13, 1};
+
+            // Act
+            rist.Capacity = 9;
+
+            // Assert
+            Assert.InRange(rist.Capacity, rist.Count, int.MaxValue);
+        }
+
+        [Fact]
         public void Capacity_ShouldThrow_WhenLessThenSize()
         {
             // Arrange
             var rist = new Rist<int> {21, 2, 8, 5, 3, 13, 1};
+
             // Assert
             Assert.Throws<ArgumentOutOfRangeException>(() => rist.Capacity = 3);
         }
