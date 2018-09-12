9a420730a1ee23d212559bd7bb05c97387f57ae5
Viktor T <qbit86@gmail.com>
2018-09-13 01:23:16 +0300

    Add description for Rist{T} class.

--- src/Misnomer.Rist/Rist.Partial.cs
+++ src/Misnomer.Rist/Rist.Partial.cs
@@ -1,9 +1,15 @@
 ﻿using System;
 using System.Buffers;
 
+// ReSharper disable once CheckNamespace
 namespace Misnomer
 {
     // https://github.com/dotnet/corert/blob/master/src/System.Private.CoreLib/shared/System/Collections/Generic/List.cs
+    /// <summary>
+    /// Represents a recyclable list of objects that can be accessed by index.
+    /// Enables pooling instances of inner array of type T[].
+    /// </summary>
+    /// <typeparam name="T">The type of elements in the list.</typeparam>
     public partial class Rist<T> : IDisposable
     {
         private const int MaxArrayLength = 0X7FEFFFFF;
