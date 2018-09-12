cb55ed8905219817e323d57d46872a013c55b115
Viktor T <qbit86@gmail.com>
2018-09-13 02:06:41 +0300

    Add documentation comments for Rist{T} members.

--- src/Misnomer.Rist/Fakes/Action.cs
+++ src/Misnomer.Rist/Fakes/Action.cs
@@ -4,5 +4,11 @@
 
 namespace Misnomer
 {
+    /// <summary>
+    /// Represents a method that converts an object from one type to another type.
+    /// </summary>
+    /// <param name="input">The object to convert.</param>
+    /// <typeparam name="TInput">The type of object that is to be converted.</typeparam>
+    /// <typeparam name="TOutput">The type the input object is to be converted to.</typeparam>
     public delegate TOutput Converter<in TInput, out TOutput>(TInput input);
 }
--- src/Misnomer.Rist/Rist.Partial.cs
+++ src/Misnomer.Rist/Rist.Partial.cs
@@ -7,7 +7,7 @@ namespace Misnomer
     // https://github.com/dotnet/corert/blob/master/src/System.Private.CoreLib/shared/System/Collections/Generic/List.cs
     /// <summary>
     /// Represents a recyclable list of objects that can be accessed by index.
-    /// Enables pooling instances of inner array of type T[].
+    /// Enables pooling instances of inner array of type <see cref="T:T[]"/>.
     /// </summary>
     /// <typeparam name="T">The type of elements in the list.</typeparam>
     public partial class Rist<T> : IDisposable
@@ -16,6 +16,9 @@ namespace Misnomer
 
         private static ArrayPool<T> Pool => ArrayPool<T>.Shared;
 
+        /// <summary>
+        /// Returns the internal array to the pool and replaces it with a zero length array.
+        /// </summary>
         public void Dispose()
         {
             T[] oldItems = _items;
@@ -25,6 +28,14 @@ namespace Misnomer
             Pool.Return(oldItems, RuntimeHelpers.IsReferenceOrContainsReferences<T>());
         }
 
+        /// <summary>
+        /// Extracts the internal array and replaces it with a zero length array.
+        /// </summary>
+        /// <returns>An <see cref="T:T[]"/> that is <see cref="Capacity"/> in length.</returns>
+        /// <remarks>
+        /// This array is <see cref="Capacity"/> in length,
+        /// so the caller should save actual <see cref="Count"/> before calling this method.
+        /// </remarks>
         public T[] MoveToArray()
         {
             T[] result = _items;
