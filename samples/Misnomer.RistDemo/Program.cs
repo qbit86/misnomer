c8499e8e9663932643f9b22d27fbc418c46a54e6
Viktor T <qbit86@gmail.com>
2018-09-15 02:36:43 +0300

    Add demo of using Rist<T>.

---
+++ samples/Misnomer.RistDemo/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using Misnomer.Extensions;
+using static System.Console;
+
+namespace Misnomer.RistDemo
+{
+    internal static class Program
+    {
+        private static void Main()
+        {
+            Rist<string> rist = Directory.EnumerateDirectories(".").ToRist();
+            rist.AddRange(Directory.EnumerateFiles("."));
+            foreach (string item in rist)
+                WriteLine(item);
+
+            int count = rist.Count;
+            int capacity = rist.Capacity;
+            string[] array = rist.MoveToArray();
+            int length = array.Length;
+            WriteLine($"{nameof(count)}: {count}, {nameof(capacity)}: {capacity}, {nameof(length)}: {length}");
+
+            int newCount = rist.Count;
+            int newCapacity = rist.Capacity;
+            WriteLine($"{nameof(newCount)}: {newCount}, {nameof(newCapacity)}: {newCapacity}");
+
+            rist.Dispose();
+        }
+    }
+}
