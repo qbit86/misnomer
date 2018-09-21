178e5cfab4bedac81978de4fcee7f4ccacc9d612
Viktor T <qbit86@gmail.com>
2018-09-22 02:35:51 +0300

    Add platform to jobs.

--- benchmarks/Misnomer.Rist.Benchmark/Program.cs
+++ benchmarks/Misnomer.Rist.Benchmark/Program.cs
@@ -13,11 +13,13 @@ namespace Misnomer
             // https://benchmarkdotnet.org/articles/configs/configs.html
             Job clrLegacyJitJob = new Job(Job.Default)
                 .With(Runtime.Clr)
+                .With(Platform.X86)
                 .With(Jit.LegacyJit)
                 .ApplyAndFreeze(RunMode.Short);
 
             Job coreRyuJitJob = new Job(Job.Default)
                 .With(Runtime.Core)
+                .With(Platform.X64)
                 .With(Jit.RyuJit)
                 .ApplyAndFreeze(RunMode.Short);
 
