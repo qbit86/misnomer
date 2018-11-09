4655be71c474ace0222ff9ac442b691fda8aa9c3
Viktor T <viktor.t***@gmail.com>
2018-11-09 11:52:20 +0300

    Add StringJoinBenchmark.

---
+++ benchmarks/Misnomer.Rist.Benchmark/StringJoinBenchmark.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+using BenchmarkDotNet.Attributes;
+
+namespace Misnomer
+{
+    public abstract class StringJoinBenchmark
+    {
+        [Benchmark(Baseline = true)]
+        public string Move()
+        {
+            return string.Empty;
+        }
+
+        [Benchmark]
+        public string Linq()
+        {
+            return string.Empty;
+        }
+    }
+}
