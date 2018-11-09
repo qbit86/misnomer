ff554c42fb22530ab535b26d603702129a95a2f3
Viktor T <viktor.t***@gmail.com>
2018-11-09 12:17:02 +0300

    Try to add argument source.

--- benchmarks/Misnomer.Rist.Benchmark/StringJoinBenchmark.cs
+++ benchmarks/Misnomer.Rist.Benchmark/StringJoinBenchmark.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using System.Collections.Generic;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
 
@@ -6,16 +6,38 @@ namespace Misnomer
 {
     public abstract class StringJoinBenchmark
     {
-        [Benchmark(Baseline = true)]
-        public string Move()
+        private static Rist<string> CreateAndPopulateList()
         {
-            return string.Empty;
+            var list = new Rist<string>();
+            // Populating, filtering, sorting, grouping...
+            list.Add("Camille");
+            list.Add("Annie");
+            list.Add("Sara");
+            list.Add("Katrin");
+            list.Add("Kari");
+
+            return list;
+        }
+
+        [Benchmark(Baseline = true)]
+        [ArgumentsSource(nameof(Data))]
+        public string Move(Rist<string> list)
+        {
+            string[] array = list.MoveToArray();
+            return string.Join(", ", array, 1, 3);
         }
 
         [Benchmark]
-        public string Linq()
+        [ArgumentsSource(nameof(Data))]
+        public string Linq(Rist<string> list)
         {
-            return string.Empty;
+            IEnumerable<string> enumerable = list.Skip(1).Take(3);
+            return string.Join(", ", enumerable);
+        }
+
+        public IEnumerable<Rist<string>> Data()
+        {
+            yield return CreateAndPopulateList();
         }
     }
 }
