bf4158fdc9e5817261f1dbedd93b0569e9b86231
Viktor T <qbit86@gmail.com>
2019-02-03 13:02:32 +0300

    Add Remove_ShouldBehaveTheSameWay().

--- tests/Misnomer.FictionaryTest/FictionaryTest.cs
+++ tests/Misnomer.FictionaryTest/FictionaryTest.cs
@@ -12,21 +12,26 @@ namespace Misnomer
     {
         private const int Count = 233;
 
-        private static KeyValuePair<int, string>[] s_keyValuePairs;
+        private static KeyValuePair<int, string>[] s_sampleItems;
+
+        private static ImmutableDictionary<int, string> s_sampleDictionary;
 
         private static double ScaleFactor { get; } = 1.0;
 
         private static double CommonRatio { get; } = Math.Pow(2.0, 1.0 / 12.0);
 
-        private static KeyValuePair<int, string>[] KeyValuePairs =>
-            s_keyValuePairs ?? (s_keyValuePairs = CreateKeyValuePairs());
+        private static KeyValuePair<int, string>[] SampleItems =>
+            s_sampleItems ?? (s_sampleItems = CreateSampleItems());
+
+        private static ImmutableDictionary<int, string> SampleDictionary =>
+            s_sampleDictionary ?? (s_sampleDictionary = CreateSampleDictionary());
 
         private static double Geometric(int i)
         {
             return Math.Pow(CommonRatio, i) * ScaleFactor;
         }
 
-        private static KeyValuePair<int, string>[] CreateKeyValuePairs()
+        private static KeyValuePair<int, string>[] CreateSampleItems()
         {
             var result = new KeyValuePair<int, string>[Count];
 
@@ -41,26 +46,28 @@ namespace Misnomer
             return result;
         }
 
+        private static ImmutableDictionary<int, string> CreateSampleDictionary()
+        {
+            ImmutableDictionary<int, string>.Builder builder =
+                ImmutableDictionary.CreateBuilder<int, string>(EqualityComparer<int>.Default);
+
+            foreach (KeyValuePair<int, string> kv in SampleItems)
+            {
+                builder[kv.Key] = kv.Value;
+            }
+
+            return builder.ToImmutable();
+        }
+
         [Fact]
         public void Create_WithDictionary()
         {
             // Arrange
-            ImmutableDictionary<int, string>.Builder builder =
-                ImmutableDictionary.CreateBuilder<int, string>(EqualityComparer<int>.Default);
-            for (int i = 0; i != Count; ++i)
-            {
-                double rawValue = Geometric(i);
-                int key = Convert.ToInt32(rawValue);
-                string value = rawValue.ToString(CultureInfo.InvariantCulture);
-                builder.TryAdd(key, value);
-            }
-
-            ImmutableDictionary<int, string> sourceDictionary = builder.ToImmutable();
             var keyComparer = new GenericEqualityComparer<int>();
 
             // Act
-            var dictionary = new Dictionary<int, string>(sourceDictionary, Int32EqualityComparer.Default);
-            var fictionary = new Fictionary<int, string, GenericEqualityComparer<int>>(sourceDictionary, keyComparer);
+            var dictionary = new Dictionary<int, string>(SampleDictionary, Int32EqualityComparer.Default);
+            var fictionary = new Fictionary<int, string, GenericEqualityComparer<int>>(SampleDictionary, keyComparer);
 
             // Assert
             Assert.Empty(dictionary.Except(fictionary));
@@ -91,6 +98,25 @@ namespace Misnomer
                 double dictionaryValue = dictionary[key];
                 double fictionaryValue = fictionary[key];
                 Assert.Equal(dictionaryValue, fictionaryValue);
+            }
+        }
+
+        [Fact]
+        public void Remove_ShouldBehaveTheSameWay()
+        {
+            // Arrange
+            var dictionary = new Dictionary<int, string>(SampleDictionary, Int32EqualityComparer.Default);
+            var fictionary = new Fictionary<int, string, GenericEqualityComparer<int>>(SampleDictionary, default);
+
+            // Act
+            int count = SampleItems.Length;
+            for (int i = 0; i != count; ++i)
+            {
+                int key = SampleItems[i].Key * (i % 2 == 0 ? 1 : -1);
+                bool d = dictionary.Remove(key);
+                bool f = fictionary.Remove(key);
+
+                Assert.Equal(d, f);
             }
         }
 
