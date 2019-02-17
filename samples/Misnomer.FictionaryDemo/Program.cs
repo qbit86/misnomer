21a2f2284f18293e0cafa9cfa770cb8efbf3fdb0
Viktor T <qbit86@gmail.com>
2019-02-18 01:55:40 +0300

    Add FictionaryDemo

--- samples/Misnomer.FictionaryDemo/Program.cs
+++ samples/Misnomer.FictionaryDemo/Program.cs
@@ -1,9 +1,29 @@
-﻿namespace Misnomer
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Misnomer.Extensions;
+
+namespace Misnomer
 {
     internal static class Program
     {
         private static void Main()
         {
+            Fictionary<string, FileSystemInfo, StringOrdinalComparer> fictionary = Directory.EnumerateDirectories(".")
+                .Select(s => KeyValuePair.Create(s, (FileSystemInfo)new DirectoryInfo(s)))
+                .ToFictionary(new StringOrdinalComparer());
+            foreach (string s in Directory.EnumerateFiles("."))
+                fictionary.Add(s, new FileInfo(s));
+
+            foreach (KeyValuePair<string, FileSystemInfo> kv in fictionary)
+                Console.WriteLine($"{kv.Value.LastWriteTimeUtc:s} {kv.Key}");
+
+            Console.WriteLine();
+            if (fictionary.TryGetValue(@".\Program.cs", out FileSystemInfo fsi) && fsi is FileInfo fi)
+                Console.WriteLine($"{fi.Name}: {fi.Length} bytes");
+
+            fictionary.Dispose();
         }
     }
 }
