42348114f275c1379c6be78de8f04efe1046c500
Viktor T <qbit86@gmail.com>
2019-02-23 22:53:49 +0300

    Add couple of benchmarks to ArraySegmentTryGetValueBenchmark

--- benchmarks/Misnomer.FictionaryBenchmark/ArraySegmentTryGetValueBenchmark.cs
+++ benchmarks/Misnomer.FictionaryBenchmark/ArraySegmentTryGetValueBenchmark.cs
@@ -2,12 +2,10 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using BenchmarkDotNet.Attributes;
-using BenchmarkDotNet.Configs;
+using Key = System.ArraySegment<int>;
 
 namespace Misnomer
 {
-    [GroupBenchmarksBy(BenchmarkLogicalGroupRule.ByCategory)]
-    [CategoriesColumn]
     public abstract class ArraySegmentTryGetValueBenchmark
     {
         // 270th prime.
@@ -15,16 +13,17 @@ namespace Misnomer
 
         private static readonly int[] s_array = new int[Count];
 
-        private Dictionary<ArraySegment<int>, int> _dictionary;
-        // private Fictionary<ArraySegment<int>, int, ArraySegmentComparerObject<int>> _fictionaryConcreteReference;
-        private Fictionary<ArraySegment<int>, int, ArraySegmentComparer<int>> _fictionaryConcreteValue;
-        // private Fictionary<ArraySegment<int>, int, EqualityComparer<ArraySegment<int>>> _fictionaryStandardPolymorphic;
-        // private Fictionary<ArraySegment<int>, int, IEqualityComparer<ArraySegment<int>>> _fictionaryVirtual;
+        private Dictionary<Key, int> _dictionary;
+        private Fictionary<Key, int, ArraySegmentComparerObject<int>> _fictionaryConcreteReference;
 
-        private static ArraySegment<int> Trial { get; } = new ArraySegment<int>(Array.Empty<int>());
+        private Fictionary<Key, int, ArraySegmentComparer<int>> _fictionaryConcreteValue;
+        // private Fictionary<Key, int, EqualityComparer<Key>> _fictionaryStandardPolymorphic;
+        // private Fictionary<Key, int, IEqualityComparer<Key>> _fictionaryVirtual;
+
+        private static Key Trial { get; } = new Key(Array.Empty<int>());
 
         private static TDictionary PopulateDictionary<TDictionary>(TDictionary dictionary)
-            where TDictionary : IDictionary<ArraySegment<int>, int>
+            where TDictionary : IDictionary<Key, int>
         {
             Debug.Assert(dictionary != null, "dictionary != null");
 
@@ -32,7 +31,7 @@ namespace Misnomer
             {
                 // 269th prime.
                 int value = 1723 * i % Count;
-                var key = new ArraySegment<int>(s_array, value, Count - value);
+                var key = new Key(s_array, value, Count - value);
                 dictionary[key] = value;
             }
 
@@ -41,51 +40,81 @@ namespace Misnomer
 
         #region GlobalSetup
 
+        [GlobalSetup(Target = nameof(DictionaryDefault))]
+        public void GlobalSetupDictionaryDefault()
+        {
+            var dictionary = new Dictionary<Key, int>();
+            _dictionary = PopulateDictionary(dictionary);
+        }
+
         [GlobalSetup(Target = nameof(DictionaryConcreteValue))]
         public void GlobalSetupDictionaryConcreteValue()
         {
-            var dictionary = new Dictionary<ArraySegment<int>, int>(new ArraySegmentComparer<int>());
+            var dictionary = new Dictionary<Key, int>(new ArraySegmentComparer<int>());
             _dictionary = PopulateDictionary(dictionary);
         }
 
         [GlobalSetup(Target = nameof(FictionaryConcreteValue))]
         public void GlobalSetupFictionaryConcreteValue()
         {
-            var fictionary = new Fictionary<ArraySegment<int>, int, ArraySegmentComparer<int>>(
-                new ArraySegmentComparer<int>());
+            var fictionary = new Fictionary<Key, int, ArraySegmentComparer<int>>(new ArraySegmentComparer<int>());
             _fictionaryConcreteValue = PopulateDictionary(fictionary);
         }
 
-        [GlobalSetup(Target = nameof(DictionaryDefault))]
-        public void GlobalSetupDictionaryDefault()
+        [GlobalSetup(Target = nameof(DictionaryConcreteReference))]
+        public void GlobalSetupDictionaryConcreteReference()
         {
-            var dictionary = new Dictionary<ArraySegment<int>, int>();
+            var dictionary = new Dictionary<Key, int>(ArraySegmentComparerObject<int>.Default);
             _dictionary = PopulateDictionary(dictionary);
+        }
+
+        [GlobalSetup(Target = nameof(FictionaryConcreteReference))]
+        public void GlobalSetupFictionaryConcreteReference()
+        {
+            var fictionary =
+                new Fictionary<Key, int, ArraySegmentComparerObject<int>>(ArraySegmentComparerObject<int>.Default);
+            _fictionaryConcreteReference = PopulateDictionary(fictionary);
+        }
+
+        [GlobalCleanup]
+        public void GlobalCleanup()
+        {
+            _dictionary = null;
+            _fictionaryConcreteValue = null;
         }
 
         #endregion
 
         #region Benchmarks
 
+        [Benchmark]
+        public bool DictionaryDefault()
+        {
+            return _dictionary.TryGetValue(Trial, out int _);
+        }
+
         [Benchmark(Baseline = true)]
-        [BenchmarkCategory("ConcreteValue")]
         public bool DictionaryConcreteValue()
         {
             return _dictionary.TryGetValue(Trial, out int _);
         }
 
         [Benchmark]
-        [BenchmarkCategory("ConcreteValue")]
         public bool FictionaryConcreteValue()
         {
             return _fictionaryConcreteValue.TryGetValue(Trial, out int _);
         }
 
         [Benchmark]
-        [BenchmarkCategory("ConcreteValue")]
-        public bool DictionaryDefault()
+        public bool DictionaryConcreteReference()
         {
             return _dictionary.TryGetValue(Trial, out int _);
+        }
+
+        [Benchmark]
+        public bool FictionaryConcreteReference()
+        {
+            return _fictionaryConcreteReference.TryGetValue(Trial, out int _);
         }
 
         #endregion
