4a707f07ffb53f8c77e91e4deb9ddc7d158725cf
Viktor T <qbit86@gmail.com>
2020-10-28 13:14:31 +0300

    Remove redundant import

--- src/Misnomer.Rist/Rist.cs
+++ src/Misnomer.Rist/Rist.cs
@@ -7,7 +7,6 @@ using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
-using System.Threading;
 
 namespace Misnomer
 {
