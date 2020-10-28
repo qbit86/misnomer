464eaecc37af151ca148f1ec18925e3bba74430d
Viktor T <qbit86@gmail.com>
2020-10-28 17:53:25 +0300

    Set `netstandard1.3` as minimum target platfotm to allow usage of Debug.Fail()

--- src/Misnomer.Internal/System/NullableAttributes.cs
+++ src/Misnomer.Internal/System/NullableAttributes.cs
@@ -1,4 +1,4 @@
-#if NETCOREAPP2_0 || NET461 || NETSTANDARD1_1 || NETSTANDARD1_2 || NETSTANDARD2_0
+#if NETCOREAPP2_0 || NET461 || NETSTANDARD1_3 || NETSTANDARD2_0
 namespace System.Diagnostics.CodeAnalysis
 {
     [AttributeUsage(AttributeTargets.Method, Inherited = false)]
