099763d0cad2d06a0e292ee2011e8b458af9d7eb
Viktor T <qbit86@gmail.com>
2020-10-29 13:05:31 +0300

    Add more nullable attributes

--- src/Misnomer.Internal/System/NullableAttributes.cs
+++ src/Misnomer.Internal/System/NullableAttributes.cs
@@ -1,6 +1,21 @@
 #if NETCOREAPP2_0 || NET461 || NETSTANDARD1_3 || NETSTANDARD2_0
 namespace System.Diagnostics.CodeAnalysis
 {
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property)]
+    internal sealed class AllowNullAttribute : Attribute { }
+
+    [AttributeUsage(
+        AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property | AttributeTargets.ReturnValue)]
+    internal sealed class MaybeNullAttribute : Attribute { }
+
+    [AttributeUsage(AttributeTargets.Parameter)]
+    internal sealed class MaybeNullWhenAttribute : Attribute
+    {
+        internal MaybeNullWhenAttribute(bool returnValue) => ReturnValue = returnValue;
+
+        internal bool ReturnValue { get; }
+    }
+
     [AttributeUsage(AttributeTargets.Method, Inherited = false)]
     internal sealed class DoesNotReturnAttribute : Attribute { }
 }
