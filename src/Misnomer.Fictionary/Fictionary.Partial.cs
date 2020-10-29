ebaa1ffe971f92208181656fc04a5708a722ea20
Viktor T <qbit86@gmail.com>
2020-10-29 13:15:01 +0300

    Update URL of the source of Fictionary https://github.com/dotnet/runtime/blob/1fddb020356e982f13195da9dd6dfc34ac2b03f1/src/libraries/System.Private.CoreLib/src/System/Collections/Generic/Dictionary.cs

--- src/Misnomer.Fictionary/Fictionary.Partial.cs
+++ src/Misnomer.Fictionary/Fictionary.Partial.cs
@@ -79,7 +79,7 @@ namespace Misnomer
 #pragma warning restore CA1000 // Do not declare static members on generic types
     }
 
-    // https://github.com/dotnet/runtime/blob/7210b177874cc8d2958a6448978c321e9d756c3a/src/libraries/System.Private.CoreLib/src/System/Collections/Generic/Dictionary.cs
+    // https://github.com/dotnet/runtime/blob/1fddb020356e982f13195da9dd6dfc34ac2b03f1/src/libraries/System.Private.CoreLib/src/System/Collections/Generic/Dictionary.cs
 
     // ReSharper disable UnusedTypeParameter
     /// <summary>
