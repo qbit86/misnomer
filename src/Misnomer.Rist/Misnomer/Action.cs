ce5aae99f0241ca25865b1464ab004236736acc8
Viktor Pti <qbit86@gmail.com>
2020-12-27 23:07:05 +0300

    Revert "Move Action from Misnomer to System namespace"
    This reverts commit 8a07b6fd56ea1c7aaeb5bd3c44ac87afb28b9428.

--- src/Misnomer.Rist/Misnomer/Action.cs
+++ src/Misnomer.Rist/Misnomer/Action.cs
@@ -1,8 +1,8 @@
 // Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
 
-#if NETCOREAPP2_1 || NET461 || NETSTANDARD1_3 || NETSTANDARD2_0
-namespace System
+namespace Misnomer
 {
     /// <summary>
     /// Represents a method that converts an object from one type to another type.
@@ -12,4 +12,3 @@ namespace System
     /// <typeparam name="TOutput">The type the input object is to be converted to.</typeparam>
     public delegate TOutput Converter<in TInput, out TOutput>(TInput input);
 }
-#endif
